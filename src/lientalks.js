cdd9aefff435b7c0e3732fd04ee26ecf6eaf2df8
Qing-Cheng Li <qc.linux@gmail.com>
2014-09-16 00:07:51 +0800

    Remove browser add-ons, rename kp to lien

--- src/lientalks.js
+++ src/lientalks.js
@@ -1,12 +1,13 @@
 (function($) {
 
-  $.kptalks = function(options) {
+  $.lientalks = function(options) {
 
     var settings = $.extend({
-        api: 'http://api.kptaipei.tw/v1/category/40?accessToken=kp53f568e77303e9.28212837',
+        api: 'http://api.lientaipei.tw/v1/category/40?accessToken=lien53f568e77303e9.28212837',
         image: [
-          'http://goooooooogle.github.io/kp/img/kp.png',
-          'http://goooooooogle.github.io/kp/img/kp-2.png'
+          'http://qcl.github.io/lien/img/lien.png',
+          'http://qcl.github.io/lien/img/lien-1.png',
+          'http://qcl.github.io/lien/img/lien-2.png'
         ],
         height: 450, // image height
         width: 466, // image width
@@ -16,20 +17,20 @@
         popup_color: 'black', // popup font color
         popup_bgcolor: 'beige', // popup background color
         readmore_color: 'brown', // popup font color
-        comein_position: 80, // show kp after scroll more than percent of page height
-        default_text: '你好，我是柯文哲', // the words show in popup before loading done
-        enter_from: 'left', // options: left, right
+        comein_position: 80, // show lien after scroll more than percent of page height
+        default_text: '你好，我是連戰的兒子連勝文', // the words show in popup before loading done
+        enter_from: 'right', // options: left, right
         enter_distance: -130 // the distance to window side
 
         //left: -130, // remove this option after ver2.0
     }, options);
 
-    createKP(settings);
+    createLien(settings);
 
-    var container = $('#kp_come_container'),
-        kp_image = $('#kp_come_container img'),
-        popup = $('#kp_popup'),
-        close = $('#kp_close_popup');
+    var container = $('#lien_come_container'),
+        lien_image = $('#lien_come_container img'),
+        popup = $('#lien_popup'),
+        close = $('#lien_close_popup');
 
     $(window).scroll(function(){
       var scroll = $(window).scrollTop(),
@@ -111,7 +112,7 @@
         }
       }
     });
-    kp_image.click(function(){
+    lien_image.click(function(){
       var come_out_forever = {};
       come_out_forever[settings.enter_from] = '-'+(settings.width+10)+'px';
       popup.remove();
@@ -123,7 +124,7 @@
     });
   };
 
-  function createKP(settings){
+  function createLien(settings){
     var img_src,arrow_pos;
     if($.isArray(settings.image)==true)
       img_src = settings.image[Math.floor(Math.random()*(settings.image.length))];
@@ -133,7 +134,7 @@
       arrow_pos = 'right';
     else
       arrow_pos = 'left';
-    var object = '<div id="kp_come_container" style="width:'+settings.width+'px; height:'+settings.height+'px; '+settings.enter_from+':-'+settings.width+'px; bottom:0;"><img src="'+img_src+'" style="width:'+settings.width+'px; height:'+settings.height+'px;"><div id="kp_popup" style="'+settings.enter_from+':'+((settings.width)*0.8)+'px;top:'+(settings.height*0.28)+'px;-webkit-border-radius:'+settings.popup_radius+';-moz-border-radius:'+settings.popup_radius+';border-radius:'+settings.popup_radius+';background-color:'+settings.popup_bgcolor+'"><div id="kp_says" style="color:'+settings.popup_color+'">'+settings.default_text+'<a href="http://kptaipei.tw" target="_blank" class="kp_readmore" style="color:'+settings.readmore_color+'">了解更多柯文哲的政見</a></div><div id="kp_popup_arrow_shadow" style="border-'+arrow_pos+': 40px solid rgba(0,0,0,.1);'+settings.enter_from+': -40px;"></div><div id="kp_popup_arrow" style="border-'+arrow_pos+': 42px solid '+settings.popup_bgcolor+';'+settings.enter_from+': -40px;"></div><div id="kp_close_popup">X</div></div></div>';
+    var object = '<div id="lien_come_container" style="width:'+settings.width+'px; height:'+settings.height+'px; '+settings.enter_from+':-'+settings.width+'px; bottom:0;"><img src="'+img_src+'" style="width:'+settings.width+'px; height:'+settings.height+'px;"><div id="lien_popup" style="'+settings.enter_from+':'+((settings.width)*0.8)+'px;top:'+(settings.height*0.28)+'px;-webkit-border-radius:'+settings.popup_radius+';-moz-border-radius:'+settings.popup_radius+';border-radius:'+settings.popup_radius+';background-color:'+settings.popup_bgcolor+'"><div id="lien_says" style="color:'+settings.popup_color+'">'+settings.default_text+'<a href="http://taipeihope.tw" target="_blank" class="lien_readmore" style="color:'+settings.readmore_color+'">了解更多連勝文的政見</a></div><div id="lien_popup_arrow_shadow" style="border-'+arrow_pos+': 40px solid rgba(0,0,0,.1);'+settings.enter_from+': -40px;"></div><div id="lien_popup_arrow" style="border-'+arrow_pos+': 42px solid '+settings.popup_bgcolor+';'+settings.enter_from+': -40px;"></div><div id="lien_close_popup">X</div></div></div>';
     $('body').append(object);
     loadData(settings);
   }
@@ -146,19 +147,19 @@
         posts[i] = item; i++;
       });
       var post = posts[Math.floor(Math.random()*(i-1))]
-          link = '<a href="'+post.url+'" target="_blank" class="kp_readmore" style="color:'+settings.readmore_color+'">了解更多柯文哲的政見</a>';
+          link = '<a href="'+post.url+'" target="_blank" class="lien_readmore" style="color:'+settings.readmore_color+'">了解更多柯文哲的政見</a>';
 
       var title = (post.title).replace(/【柯p新政】/g,"");
       var content = stringReplace(post.plain_content);
       content = content.split('\n');
       if(content[1] == undefined){
-        says = '<p id="kp_say_bighi" style="color:'+settings.popup_color+'">'+settings.default_text+'<br>我提出<br>「'+title.substring(2)+'」</p>'+link;
+        says = '<p id="lien_say_bighi" style="color:'+settings.popup_color+'">'+settings.default_text+'<br>我提出<br>「'+title.substring(2)+'」</p>'+link;
       }
       else {
-        says = '<p id="kp_say_hi" style="color:'+settings.popup_color+'">'+settings.default_text+'</p>'+content[3]+content[4]+'...'+link+'</p>';
+        says = '<p id="lien_say_hi" style="color:'+settings.popup_color+'">'+settings.default_text+'</p>'+content[3]+content[4]+'...'+link+'</p>';
       }
 
-      $('#kp_says').scrollTop(0).html(says).promise().done(function(){
+      $('#lien_says').scrollTop(0).html(says).promise().done(function(){
         $('p').removeAttr("style"); $('span').removeAttr("style");
       });
     });
@@ -179,27 +180,27 @@
     switch(effect) {
 
       case 'fade':
-        $('#kp_popup').fadeIn('slow');
+        $('#lien_popup').fadeIn('slow');
         break;
 
       case 'slide':
-        $('.kp_readmore').hide(function(){
-          $('#kp_popup').slideDown('fast',function(){
-            $('.kp_readmore').fadeIn();
+        $('.lien_readmore').hide(function(){
+          $('#lien_popup').slideDown('fast',function(){
+            $('.lien_readmore').fadeIn();
           });
         });
         break;
 
       case 'zoom':
-        $('#kp_says').hide(function(){
-          $('#kp_popup').show('slow',function(){
-            $('#kp_says').fadeIn();
+        $('#lien_says').hide(function(){
+          $('#lien_popup').show('slow',function(){
+            $('#lien_says').fadeIn();
           });
         });
         break;
 
       default:
-        $('#kp_popup').show();
+        $('#lien_popup').show();
         break;
     }
   }
